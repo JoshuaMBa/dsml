e23973dc630b0cbfa2ba095e04a78f0c1e29ddbb
Joshua Baehring <josh.baehring@gmail.com>
2024-12-24 05:28:10 +0000

    Coordinator test update.

--- gpu_sim/tests/coordinator_test.go
+++ gpu_sim/tests/coordinator_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	pb "github.com/JoshuaMBa/dsml/gpu_sim/proto"
-	"github.com/stretchr/testify/assert"
+	util "github.com/JoshuaMBa/dsml/utils"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -23,7 +23,7 @@ func TestAllReduceRingBasic(t *testing.T) {
 	coordinatorClient := pb.NewGPUCoordinatorClient(coordinatorConn)
 
 	numDevices := uint32(3)
-	fmt.Printf("Requesting a communicator with %d devices...\n", numDevices)
+	log.Printf("Requesting a communicator with %d devices...\n", numDevices)
 	ctx := context.Background()
 
 	commInitResponse, err := coordinatorClient.CommInit(ctx, &pb.CommInitRequest{
@@ -34,29 +34,77 @@ func TestAllReduceRingBasic(t *testing.T) {
 	}
 
 	if commInitResponse.Success {
-		fmt.Printf("CommInit succeeded. CommId: %d\n", commInitResponse.CommId)
+		log.Printf("CommInit succeeded. CommId: %d\n", commInitResponse.CommId)
 	} else {
-		fmt.Println("CommInit failed.")
-		return
+		log.Fatalf("CommInit failed.")
 	}
 
+	log.Printf("Initializing data on GPUs...\n")
+	expected := make([]float64, numDevices)
 	memAddrs := make(map[uint32]*pb.MemAddr)
 	for i, data := range commInitResponse.Devices {
 		memAddrs[uint32(i)] = &pb.MemAddr{
 			Value: data.MinMemAddr.Value,
 		}
+
+		var bytes []byte
+		for j := 0; j < int(numDevices); j++ {
+			expected[j] += float64(i + j)
+			bytes = append(bytes, util.Float64ToBytes(float64(i+j))...)
+		}
+		_, err := coordinatorClient.Memcpy(ctx, &pb.MemcpyRequest{
+			Either: &pb.MemcpyRequest_HostToDevice{
+				HostToDevice: &pb.MemcpyHostToDeviceRequest{
+					HostSrcData: bytes,
+					DstDeviceId: &pb.DeviceId{Value: data.DeviceId.Value},
+					DstMemAddr:  &pb.MemAddr{Value: data.MinMemAddr.Value},
+				},
+			},
+		})
+
+		if err != nil {
+			log.Fatalf("Failed to copy initial data to device of rank 0")
+		}
 	}
 
-	allReduceRingResponse, err := coordinatorClient.AllReduceRing(
+	log.Printf("Initiating Ring Reduce...\n")
+	_, err = coordinatorClient.AllReduceRing(
 		ctx,
 		&pb.AllReduceRingRequest{
 			CommId:   commInitResponse.CommId,
-			Count:    3,
+			Count:    uint64(numDevices),
 			Op:       pb.ReduceOp_SUM,
 			MemAddrs: memAddrs,
 		},
 	)
-	log.Printf("Error: %v", err)
-	assert.Equal(t, nil, err)
-	assert.Equal(t, true, allReduceRingResponse.Success)
+	if err != nil {
+		log.Fatalf("AllReduceRing failed")
+	}
+
+	wordSize := uint64(8)
+	res, err := coordinatorClient.Memcpy(ctx, &pb.MemcpyRequest{
+		Either: &pb.MemcpyRequest_DeviceToHost{
+			DeviceToHost: &pb.MemcpyDeviceToHostRequest{
+				NumBytes:    uint64(numDevices) * wordSize,
+				SrcDeviceId: &pb.DeviceId{Value: commInitResponse.Devices[0].DeviceId.Value},
+				SrcMemAddr:  &pb.MemAddr{Value: commInitResponse.Devices[0].MinMemAddr.Value},
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Failed to retrieve final data from device of rank 0 with deviceId %v", commInitResponse.Devices[0].DeviceId.Value)
+	}
+
+	s := "got:"
+	for i := 0; i < int(numDevices); i++ {
+		f := res.GetDeviceToHost().DstData[uint64(i)*wordSize : (uint64(i)*wordSize)+wordSize]
+		s += fmt.Sprintf("%v ", util.BytesToFloat64(f))
+	}
+	log.Printf(s + "\n")
+
+	s = "expected:"
+	for i := 0; i < int(numDevices); i++ {
+		s += fmt.Sprintf("%v ", expected[i])
+	}
+	log.Printf(s + "\n")
 }
